e098840e3889579b9ea32884c118d0748dcfad4b
reza-chilli <reza.felfelian@gmail.com>
2023-07-13 18:28:25 +0430

    use moment.js for dates

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,5 @@ import '../scss/style.scss';
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
 var $ = window.$ = require('jquery');
+var moment = window.moment = require('moment');
 import 'datatables.net-bs5';
