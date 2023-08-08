b1804035e319ccbbd18eb0e16f778804f1df77f6
reza-chilli <reza.felfelian@gmail.com>
2023-08-08 22:58:08 +0430

    autoComplete product when creating new project

--- controllers/product.js
+++ controllers/product.js
@@ -1,122 +1,141 @@
-const productService = require('../services/product');
+const productService = require("../services/product");
 
 module.exports = {
-    productDataTableRender: (req, res) => {
-        const data = {
-            error: req.flash('error'),
-            success: req.flash('success'),
-        };
-        return res.render('products/product', data);
-    },
-    addProductRender: (req, res) => {
-        const data = {
-            error: req.flash('error'),
-            success: req.flash('success'),
-            code: null,
-            title: null,
-        };
-        return res.render('products/add', data);
-    },
-    addProductPostData: async (req, res) => {
-        try {
-            const data = req.body;
-            const newProduct = await productService.create(data);
+  productDataTableRender: (req, res) => {
+    const data = {
+      error: req.flash("error"),
+      success: req.flash("success"),
+    };
+    return res.render("products/product", data);
+  },
+  addProductRender: (req, res) => {
+    const data = {
+      error: req.flash("error"),
+      success: req.flash("success"),
+      code: null,
+      title: null,
+    };
+    return res.render("products/add", data);
+  },
+  addProductPostData: async (req, res) => {
+    try {
+      const data = req.body;
+      const newProduct = await productService.create(data);
 
-            res.status(200).send(String(newProduct._id));
-        } catch (error) {
-            console.error(`Error in addProductPostData: ${error}`);
-        }
-    },
-    async productRender(req, res) {
-        try {
-            const { productId } = req.params;
-            let { title, code } = await productService.findOne({ _id: productId });
-            code = code.toString();
-            const codeLength = code.length;
-            if (code.length < 3) {
-                for (let i = 0; i < 3 - codeLength; i++) {
-                    code = '0' + code;
-                }
-            }
-            const data = {
-                error: req.flash('error'),
-                success: req.flash('success'),
-                title,
-                code,
-            };
-            res.render('products/add', data);
-        } catch (error) {
-            console.error(`Error in product render: ${error}`);
-        }
-    },
-    async deleteProductPostData(req, res) {
-        try {
-            const data = req.body;
-            const query = {
-                _id: { $in: data.ids },
-            };
-            const deleteProducts = await productService.deleteMany(query);
-            res.status(200).send();
-        } catch (error) {
-            console.error(`Error in deleteProductPostData: ${error}`);
-        }
-    },
-    async deactivateProductPostData(req, res) {
-        try {
-            const body = req.body;
-            const data = {
-                _id: { $in: body.ids },
-            };
-            const query = { active: false };
-            const deactiveProducts = await productService.updateMany(data, query);
-            res.status(200).send();
-        } catch (error) {
-            console.error(`Error in deactivateProductPostData: ${error}`);
-        }
-    },
-    async activateProductPostData(req, res) {
-        try {
-            const body = req.body;
-            const data = {
-                _id: { $in: body.ids },
-            };
-            const query = { active: true };
-            const deactiveProducts = await productService.updateMany(data, query);
-            res.status(200).send();
-        } catch (error) {
-            console.error(`Error in activateProductPostData: ${error}`);
-        }
-    },
-    async productDataTable(req, res) {
-        try {
-            const start = parseInt(req.body.start);
-            const length = parseInt(req.body.length);
-            const search = req.body.search.value;
-            const sanitizedSearch = escape(search);
-            const query = {};
-
-            if (search !== '') {
-                query.title = { $regex: `.*${sanitizedSearch}.*` };
-            }
-            if (req.body.status == 'active') {
-                query.active = true;
-            } else if (req.body.status == 'deactive') {
-                query.active = false;
-            }
-
-            const productCount = await productService.countDocuments({});
-            const filteredProductCount = await productService.countDocuments(query);
-
-            const products = await productService.find(query, null, { skip: start, limit: length });
-            const obj = {
-                draw: req.body.draw,
-                recordsTotal: productCount,
-                recordsFiltered: filteredProductCount,
-                data: products,
-            };
-            res.status(200).send(obj);
-        } catch (error) {
-            console.error(`Error in productDataTable: ${error}`);
-        }
+      res.status(200).send(String(newProduct._id));
+    } catch (error) {
+      console.error(`Error in addProductPostData: ${error}`);
     }
-}
+  },
+  async productRender(req, res) {
+    try {
+      const { productId } = req.params;
+      let { title, code } = await productService.findOne({ _id: productId });
+      code = code.toString();
+      const codeLength = code.length;
+      if (code.length < 3) {
+        for (let i = 0; i < 3 - codeLength; i++) {
+          code = "0" + code;
+        }
+      }
+      const data = {
+        error: req.flash("error"),
+        success: req.flash("success"),
+        title,
+        code,
+      };
+      res.render("products/add", data);
+    } catch (error) {
+      console.error(`Error in product render: ${error}`);
+    }
+  },
+  async deleteProductPostData(req, res) {
+    try {
+      const data = req.body;
+      const query = {
+        _id: { $in: data.ids },
+      };
+      const deleteProducts = await productService.deleteMany(query);
+      res.status(200).send();
+    } catch (error) {
+      console.error(`Error in deleteProductPostData: ${error}`);
+    }
+  },
+  async deactivateProductPostData(req, res) {
+    try {
+      const body = req.body;
+      const data = {
+        _id: { $in: body.ids },
+      };
+      const query = { active: false };
+      const deactiveProducts = await productService.updateMany(data, query);
+      res.status(200).send();
+    } catch (error) {
+      console.error(`Error in deactivateProductPostData: ${error}`);
+    }
+  },
+  async activateProductPostData(req, res) {
+    try {
+      const body = req.body;
+      const data = {
+        _id: { $in: body.ids },
+      };
+      const query = { active: true };
+      const deactiveProducts = await productService.updateMany(data, query);
+      res.status(200).send();
+    } catch (error) {
+      console.error(`Error in activateProductPostData: ${error}`);
+    }
+  },
+  async productDataTable(req, res) {
+    try {
+      const start = parseInt(req.body.start);
+      const length = parseInt(req.body.length);
+      const search = req.body.search.value;
+      const sanitizedSearch = escape(search);
+      const query = {};
+
+      if (search !== "") {
+        query.title = { $regex: `.*${sanitizedSearch}.*` };
+      }
+      if (req.body.status == "active") {
+        query.active = true;
+      } else if (req.body.status == "deactive") {
+        query.active = false;
+      }
+
+      const productCount = await productService.countDocuments({});
+      const filteredProductCount = await productService.countDocuments(query);
+
+      const products = await productService.find(query, null, {
+        skip: start,
+        limit: length,
+      });
+      const obj = {
+        draw: req.body.draw,
+        recordsTotal: productCount,
+        recordsFiltered: filteredProductCount,
+        data: products,
+      };
+      res.status(200).send(obj);
+    } catch (error) {
+      console.error(`Error in productDataTable: ${error}`);
+    }
+  },
+  async getAvailableProductPostData(req, res) {
+    try {
+      const { title } = req.body;
+      if (!title || title === "") {
+        return res.status(400).send("empty title.");
+      }
+      const query = {
+        title: { $regex: `.*${title}.*` },
+      };
+      const select = { title: 1 };
+      const products = await productService.find(query, select);
+      res.status(200).send(products);
+    } catch (error) {
+      res.status(500).send("Internal Server Error.");
+    }
+  },
+};
