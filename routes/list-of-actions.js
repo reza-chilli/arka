f8fad997361807daf1e971cbd39e8aa0816854b6
reza-chilli <reza.felfelian@gmail.com>
2023-08-25 21:34:46 +0430

    add list of action

--- routes/list-of-actions.js
+++ routes/list-of-actions.js
@@ -5,7 +5,7 @@ const listOfActionsController = require('../controllers/list-of-actions');
 router.get('/', listOfActionsController.actionsDataTableRender);
 router.post('/datatable', listOfActionsController.actionsDataTable);
 router.get('/add-action', listOfActionsController.addActionRender);
-// router.post('/add-project', projectMiddleware.addProjectPostValidate, projectController.addProjectPostData);
+router.post('/add-action', projectMiddleware.addProjectPostValidate, projectController.addProjectPostData);
 // router.get('/add-project/:projectId', projectController.projectRender);
 // router.post('/delete-project', projectMiddleware.deleteProjectPostValidate, projectController.deleteProjectPostData);
 // router.post('/deactivate-project', projectMiddleware.deactivateProjectPostValidate, projectController.deactivateProjectPostData);
